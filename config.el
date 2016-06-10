a8872a088cdec6904e0655b2e9b7bcc1be20ef33
cydparser <cydparser@gmail.com>
2016-06-10 12:55:43 -0700

    Define haskell-modes (#1)
    The variable is not yet in spacemacs master.

--- config.el
+++ config.el
@@ -1,6 +1,7 @@
 (configuration-layer/declare-layer 'haskell)
 
 (setq haskell-enable-ghc-mod-support nil
-      haskell-enable-ghci-ng-support nil)
+      haskell-enable-ghci-ng-support nil
+      haskell-modes '(haskell-mode literate-haskell-mode))
 
 (spacemacs|defvar-company-backends intero-repl-mode)
